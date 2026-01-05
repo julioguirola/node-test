65e9bd4814d9cd7a2b4fe3dff7c01f0e30adb1e3
Julio Guirola <julioguirola03@gmail.com>
2026-01-05 00:18:04 -0500

    Update API endpoint and simplify response logging

--- index.js
+++ index.js
@@ -1,27 +1,12 @@
-const AI_API_KEY = process.env.AI_API_KEY;
-const AI_API_URL = "https://generativelanguage.googleapis.com/v1beta/models/gemini-2.5-flash:generateContent"
-const BODY = {
-    "contents": [
-      {
-        "parts": [
-          {
-            "text": "Dime una curiosidad."
-          }
-        ]
-      }
-    ]
-  }
+const AI_API_URL = "https://uselessfacts.jsph.pl/api/v2/facts/random"
 let runnig = false;
 
 setInterval(async () => {
   if (runnig) return;
   const res = await fetch(AI_API_URL, {
-        body: JSON.stringify(BODY),
         headers: {
             "Content-Type": "application/json",
-            "x-goog-api-key": AI_API_KEY,
-        },
-        method: "POST",
+        }
     })
 
     if (res.status !== 200) {
@@ -30,5 +15,5 @@ setInterval(async () => {
     }
 
     const data = await res.json()
-    console.log(data.candidates[0].content.parts[0].text)
+    console.log(data.text)
 }, 10 * 1000)
